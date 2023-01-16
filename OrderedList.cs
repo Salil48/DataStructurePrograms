3aa0528274fbf33653178ae6f932789c28e505df
“Salil48” <salilchharia@gmail.com>
2023-01-16 16:32:17 +0530

    Ordered List

---
+++ OrderedList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DSProgram
+{
+    class OrderedList
+    {
+        public static void Ordered()
+        {
+            string text = File.ReadAllText(@"J:\240\DSProgram\DSProgram\NumberList1.txt");
+            string[] textArray = text.Split(" ");
+            CustomLinkedList<int> list = new CustomLinkedList<int>();
+
+            int[] arr = new int[textArray.Length];
+            for (int i = 0; i < textArray.Length; i++)
+            {
+                int a = Convert.ToInt32(textArray[i]);
+                arr[i] = a;
+            }
+            for (int i = 0; i < textArray.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+                //Console.WriteLine("\n");
+            }
+            for (int i = 0; i < textArray.Length; i++)
+            {
+                list.Sort(arr[i]);
+            }
+            list.Display();
+            Console.WriteLine("\nEnter a number to search");
+            int searchNum = Convert.ToInt32(Console.ReadLine());
+            if (list.Search(searchNum))
+            {
+                list.DeleteElement(searchNum);
+
+            }
+            else
+            {
+                list.Sort(searchNum);
+            }
+            string textFile = list.ReturnString();
+            File.WriteAllText(@"J:\240\DSProgram\DSProgram\NumberList.txt", textFile);
+            list.Display();
+
+        }
+    }
+}
