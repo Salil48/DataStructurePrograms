e8cb7c2f6450bd635524bf9dd1039cee2a4827e3
“Salil48” <salilchharia@gmail.com>
2023-01-16 15:37:51 +0530

    Welcome to Data Structure Program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DSProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Structure Programs!");
+            Console.WriteLine("1.Unordered List");
+            int check = Convert.ToInt32(Console.ReadLine());
+            switch (check)
+            {
+                case 1:
+                    UnorderedList.Unordered();
+                    break;
+
+            }
+
+        }
+    }
+}
