7995e82a388d8862cc266fc9021fa09a6c21a787
“Salil48” <salilchharia@gmail.com>
2023-01-16 16:32:38 +0530

    Ordered List

--- Program.cs
+++ Program.cs
@@ -6,16 +6,19 @@
         {
             Console.WriteLine("Data Structure Programs!");
             Console.WriteLine("1.Unordered List");
+            Console.WriteLine("2.ordered List");
             int check = Convert.ToInt32(Console.ReadLine());
             switch (check)
             {
                 case 1:
                     UnorderedList.Unordered();
                     break;
+                case 2:
+                    OrderedList.Ordered();
+                    break;
 
             }
 
         }
-
     }
 }
